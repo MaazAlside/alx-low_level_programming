cdcacf1a247e1c312061579943c0e7baa9cdaa79
MaazAlside <maazalside@gmail.com>
2022-12-15 20:37:07 +0300

    solve 3 taskes 0. isupper is  a function that checks for uppercase character , 1. isdigit is  a function that checks for a digit (0 through 9),and 2. Collaboration is multiplication is a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a :  an integer parameter
+ * @b : an integer parameter
+ * Return: return a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+#endif
