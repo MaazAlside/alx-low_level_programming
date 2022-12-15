4ddbb4bfbc0e64bb9b2af9b5926baeb7dd756b12
MaazAlside <maazalside@gmail.com>
2022-12-16 00:49:39 +0300

    5-main.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
 int _putchar(char c);
 
 int _isupper(int c);
@@ -11,5 +12,7 @@ int mul(int a, int b);
 void print_numbers(void);
 
 void print_most_numbers(void);
+
+void more_numbers(void);
 
 #endif
