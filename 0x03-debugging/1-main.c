4ee4c84d90a89ab717f7242dc75a26b709beca0b
MaazAlside <maazalside@gmail.com>
2022-12-16 03:34:50 +0300

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,14 @@
 
 int main(void)
 {
-	int i;
+	int i = 0;
 
 	printf("Infinite loop incoming :(\n");
-	i = 0;
-
 	/*while (i < 10)*/
-        /*{*/
+	/*{*/
 	/*	putchar(i);*/
-        /*}*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-        return (0);
+	return (0);
 }
