8d7385b24a93c51dfe270b32ed0ad012dec0cd67
MaazAlside <maazalside@gmail.com>
2023-01-02 22:26:28 +0300

    function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_memset -  function that fills memory with a constant byte.
+ *@dest: pointer var type char
+ *@src: pointer var type char
+ *@n: int var
+ *Return: return pointer char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
