de6a0401890b762d1523c9b8193a813109656976
MaazAlside <maazalside@gmail.com>
2023-01-17 08:05:37 +0300

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef HEDER_FILE
 #define HEDER_FILE
-struct dog 
+
+/**
+ *struct dog - new struct with 3 elements
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: onwer of the dog
+ *
+ * Description: struct dog with the following elements:
+ * name, age, and owner
+ */
+
+struct dog
 {
 	char *name;
 	float age;
