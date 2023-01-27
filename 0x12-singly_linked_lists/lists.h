d1c2edae58c1d23ea74cdeb6899bd46510d84554
MaazAlside <maazalside@gmail.com>
2023-01-27 21:11:45 +0300

    function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 /**
   * struct list_s - singly linked list
   * @str: string - (malloc'ed string)
