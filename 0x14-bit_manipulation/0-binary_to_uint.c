47a18145980bf3a595e7a1f821ade413eb8b32b6
MaazAlside <maazalside@gmail.com>
2023-02-04 00:32:06 +0300

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,16 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int val = 0;
+	unsigned int result = 0;
 	int i = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	for (i = 0; b[i] != '\0' i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		result = (result << 1) | (b[i] - '0');
 	}
-	return (val);
+	return (result);
 }
