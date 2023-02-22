938fc11c4ab28bfa762d378b34bea84c4b74e8c8
MaazAlside <maazalside@gmail.com>
2023-02-23 00:02:14 +0300

    100-get_endianness.c created

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * ins - this function checks foe endianness
+  * Return: 0 if big endian and 1 if little endian
+  */
+get_endianness(void)
+{
+	int i = 1;
+	char *c = (char *)&i;
+
+	return ((*c == 1) ? 1 : 0);
+}
