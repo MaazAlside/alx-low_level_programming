bd8b01b7034fd12ab853dffa003a6926866d1575
maazalside <maazalside@gmail.com>
2023-09-28 23:12:26 +0300

    fix 0-binary_to_unit.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,11 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum = 0;
 	size_t i;
 
+	if (b == NULL)
+		return (0);
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b == NULL)
-			return (0);
-
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
