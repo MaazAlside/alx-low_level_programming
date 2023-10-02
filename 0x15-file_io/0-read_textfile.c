d95ece1a4841d5e4dd4f909614effe7c8167d6d8
maazalside <maazalside@gmail.com>
2023-10-03 02:09:01 +0300

    fix betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,59 +8,47 @@
  * Return: the total number of characters read, or 0 if an error occurs
  * or if the file cannot be opened
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    size_t i;
-    FILE *file;
-    size_t bytesRead;
+	char *buffer;
+	size_t i;
+	FILE *file;
+	size_t bytesRead;
 
-    if (filename == NULL)
-    {
-        return 0;
-    }
+	if (filename == NULL)
+		return (0);
 
-    
-    file = fopen(filename, "r");
+	file = fopen(filename, "r");
 
-    if (file == NULL)
-    {
-        return 0;
-    }
+	if (file == NULL)
+		return (0);
 
+	buffer = (char *)malloc(letters * sizeof(char));
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
 
-    buffer = (char *)malloc(letters * sizeof(char));
-    if (buffer == NULL)
-    {
-        fclose(file);
-        return 0;
-    }
+	bytesRead = 0;
 
-    bytesRead = 0;
+	for (i = 0; i < letters; i++)
+	{
+		int c = fgetc(file);
 
-    for (i = 0; i < letters; i++)
-    {
-        int c = fgetc(file);
+		if (c == EOF)
+			break;
 
-        if (c == EOF)
-        {
-            break;
-        }
-        buffer[i] = (char)c;
-        bytesRead++;
-    }
+		buffer[i] = (char)c;
+		bytesRead++;
+	}
 
-    fclose(file);
+	fclose(file);
 
+	for (i = 0; i < bytesRead; i++)
+		printf("%c", buffer[i]);
 
-    for (i = 0; i < bytesRead; i++)
-    {
-        printf("%c", buffer[i]);
-    }
+	free(buffer);
 
-
-    free(buffer);
-
-    return bytesRead;
+	return (bytesRead);
 }
