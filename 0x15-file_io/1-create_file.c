069993d6272e7d0aa56f77e63773a5875f810aab
maazalside <maazalside@gmail.com>
2023-10-03 03:18:23 +0300

    add more test.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <string.h>
+
 
 /**
- * create_file - Creates a file with a given content.
- * @filename: The name of the file to create.
- * @text_content: The content to write into the file.
- * Return: 1 on success, -1 on failure.
- */
+* create_file - Creates a file with a given content.
+* @filename: The name of the file to create.
+* @text_content: The content to write into the file.
+* Return: 1 on success, -1 on failure.
+*/
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t bytesWritten;
 	int fileDescriptor;
+
+	ssize_t bytesWritten;
+	size_t i;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,34 +22,26 @@ int create_file(const char *filename, char *text_content)
 
 	if (fileDescriptor == -1)
 	{
+		perror("Error opening file");
 		return (-1);
-	}
-
-
-
-	bytesWritten = write(fileDescriptor, text_content,
-	strlen((char *)text_content));
-
-	if (bytesWritten == -1)
-	{
-		close(fileDescriptor);
-		return (1);
 	}
 
 	if (text_content != NULL)
 	{
-		bytesWritten = write(fileDescriptor, text_content, strlen(text_content));
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			bytesWritten = write(fileDescriptor, &text_content[i], 1);
 
-        if (bytesWritten == -1)
-        {
-            close(fileDescriptor);
-            return (-1);
-        }
+			if (bytesWritten == -1)
+			{
+				perror("Error writing to file");
+				close(fileDescriptor);
+				return (-1);
+			}
+		}
 	}
-
 
 	close(fileDescriptor);
 
 	return (1);
-
 }
