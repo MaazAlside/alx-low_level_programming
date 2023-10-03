fc7267aad3b9f896d2335bdb9dd69ce91f7f90d4
maazalside <maazalside@gmail.com>
2023-10-03 03:05:36 +0300

    add more test.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL,
+	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
 	S_IRUSR | S_IWUSR);
 
 	if (fileDescriptor == -1)
 	{
-		perror("Error opening file");
-		return (1);
+		return (-1);
 	}
 
 
@@ -31,9 +30,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (bytesWritten == -1)
 	{
-		perror("Error writing to file");
 		close(fileDescriptor);
 		return (1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytesWritten = write(fileDescriptor, text_content, strlen(text_content));
+
+        if (bytesWritten == -1)
+        {
+            close(fileDescriptor);
+            return (-1);
+        }
 	}
 
 
