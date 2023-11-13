7d92de675f4c10c4097569234c97e4fb661a2e64
maazalside <maazalside@gmail.com>
2023-11-14 02:29:19 +0300

    still trying

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,11 @@
-#ifndef __HEADER__
-#define __HEADER__
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _islower(int c);
