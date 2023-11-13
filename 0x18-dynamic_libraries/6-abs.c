116bcc01c12dfcc54cba98ca942961399d6a59e2
maazalside <maazalside@gmail.com>
2023-11-14 02:08:06 +0300

    create dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @a : an integer parameter
+ * Return: return integer
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	{
+		return (a * -1);
+	}
+
+}
