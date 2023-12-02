92cad4850f7d6b18b78f8cb1fad4735dc87746fd
maazalside <maazalside@gmail.com>
2023-12-02 10:33:50 +0300

    fix eerer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,15 +18,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!size)
 		return (NULL);
 
-	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
 	if (!table)
 		return (NULL);
 
-
 	table->size = size;
-	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * table->size);
-	if (!table->arry)
+	table->array = malloc(sizeof(hash_node_t *) * table->size);
+	if (!table->array)
 		return (NULL);
 
 	for (i = 0; i < table->size; i++)
