bcccfd87fe9259b93a6128dda09e5f79a9c90c81
maazalside <maazalside@gmail.com>
2024-02-02 00:38:24 +0300

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Island Perimeter"""
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    perimeter = 0;
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row > 0 and grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row < len(grid) - 1 and grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col > 0 and grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col < len(grid[row]) - 1 and grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
